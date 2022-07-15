058266f6b214fd78a9f3439b19aac9102b11d764
maximkwski <makkie.makowski@gmail.com>
2022-07-15 13:38:53 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_025612) do
+ActiveRecord::Schema.define(version: 2022_07_13_172103) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name", null: false
